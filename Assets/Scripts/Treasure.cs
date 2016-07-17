97bf8bea982fdc9878b55f821333be837c44c67c
Mike Thal <mikeathal@gmail.com>
2016-07-16 20:38:02 -0400

    Update with UI text box. Win message is now working.

---
+++ Assets/Scripts/Treasure.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Treasure : MonoBehaviour {
+
+	bool didPlayerWin = false;
+	public Text OnScreenText;             // assign in inspector, Text UI
+	public Transform PlayerCharacter;         // assign in inspector, player
+
+	void Update () {
+
+		if ( (PlayerCharacter.position - transform.position).magnitude < 5f) {
+
+			OnScreenText.text = "Press [SPACE] to get treasure and win!";
+			if ( Input.GetKey (KeyCode.Space) ) {
+				didPlayerWin = true;
+			}
+			if ( didPlayerWin ) {
+				OnScreenText.text = "YOU GOT THE TREASURE AND YOU WIN!!";
+			}
+		} 
+	}
+}
